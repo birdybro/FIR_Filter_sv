77ad8696c1d0859fce9a96ffd58725a3ec9f8460
birdybro <kcoleman@misterfpga.co>
2023-09-17 18:17:12 -0600

    Revert as I still dont' have audio
    Reverting back to the beginning, gonna rework this later...

--- fir_filter.sv
+++ fir_filter.sv
@@ -26,128 +26,108 @@
 // The ringing artifacts are likely due to the gibbs phenomenon
 // https://en.wikipedia.org/wiki/Gibbs_phenomenon
 //
+//
+// Coefficients calculated with the following example python code:
+//
+// import numpy as np
+// import matplotlib.pyplot as plt
+// from scipy.signal import firwin, freqz
+//
+// # Filter specifications
+// num_taps = 20  # number of filter coefficients (taps)
+// cutoff_hz = 60e3  # cutoff frequency, e.g., 80 kHz
+// fs = 300e3  # sampling rate, e.g., 300 kHz
+//
+// # Design the filter using firwin
+// coefficients = firwin(num_taps, cutoff_hz, fs=fs, window="barthann")
+//
+// # Print the coefficients
+// print("FIR filter coefficients:")
+// print(coefficients)
+//
+// # Optional: Plot the frequency response
+// w, h = freqz(coefficients, worN=8000)
+// plt.plot(0.5 * fs * w / np.pi, np.abs(h), 'b')
+// plt.title('FIR Filter Frequency Response')
+// plt.xlabel('Frequency [Hz]')
+// plt.ylabel('Gain')
+// plt.grid()
+// plt.show()
+//
+//
+// Scaling for coefficients done with the following example python code:
+//
+// coeffs = [-0.00000000e+00, -1.65280117e-03,  2.06185994e-18,  1.16875861e-02,
+//            1.33504934e-02, -2.26262259e-02, -5.98970277e-02,  1.28491969e-17,
+//            1.87164918e-01,  3.71973057e-01,  3.71973057e-01,  1.87164918e-01,
+//            1.28491969e-17, -5.98970277e-02, -2.26262259e-02,  1.33504934e-02,
+//            1.16875861e-02,  2.06185994e-18, -1.65280117e-03, -0.00000000e+00]
+//
+// scaled_coeffs = [round(c * (2**15)) for c in coeffs]
+// print(scaled_coeffs)
 
-module fir_filter #(
-    parameter IW=16,
-    parameter FILTER_LENGTH=20, // (AKA number of taps)
-    parameter MCLK_RATE=53693175,
-    parameter DATA_CLK_IN=300000,
-    parameter DATA_CLK_OUT=48000
-)
-(
-    input  logic          clk,                             // 53.693175MHz Clock
-    input  logic          reset,                           // Active high reset
-    input  logic [IW-1:0] coefficients[FILTER_LENGTH-1:0], // Scaled coefficients array with fixed-point values
-    input  logic [IW-1:0] data_in,                         // 16-bit input data
-    output logic [IW-1:0] data_out                         // 16-bit output data
+
+module fir_filter (
+    input  logic        clk,     // 53.693175MHz clock assumed by example
+    input  logic        reset,   // Active high reset
+    input  logic [15:0] data_in, // 16-bit input data
+    output logic [15:0] data_out // 16-bit output data
 );
 
-// Counter values for clock division for input and output data sampling rates
-localparam SAMPLE_RATE_IN  = MCLK_RATE / DATA_CLK_IN;  // Sample rate divider for incoming data
-localparam SAMPLE_RATE_OUT = MCLK_RATE / DATA_CLK_OUT; // Sample rate divider for outgoing data
-logic [31:0] counter_in;                               // Counter with enough bits to count to SAMPLE_RATE_IN
-logic [31:0] counter_out;                              // Counter with enough bits to count to SAMPLE_RATE_OUT
-logic in_sample_valid, out_sample_valid;   // Indicates when a new sample should be processed
+// Counter values - 53.693175MHz to get 300kHz sampling rate requires 178.97725 counter
+localparam COUNTER_MAX = 178;   // Assuming 178.97725 gets truncated to 178, almost 50% duty cycle
+logic [7:0] counter;            // Counter with enough bits to count to COUNTER_MAX
+logic sample_valid;             // Indicates when a new sample should be processed
+
+// Define your filter length
+localparam FILTER_LENGTH = 20; // Adjust according to your coefficients length
+logic [15:0] COEFFICIENTS[FILTER_LENGTH-1:0] = '{0, -54, 0, 383, 437, -741, -1963, 0, 6133, 12189, 12189, 6133, 0, -1963, -741, 437, 383, 0, -54, 0};
+
+logic [15:0] x[FILTER_LENGTH-1:0];             // Array to store past input values
+logic [31:0] mult_results[FILTER_LENGTH-1:0];  // Storing the multiplication results
+logic [31:0] sum_result;                       // 32-bit result after summing
 
 always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-        counter_in       <= '0;
-        counter_out      <= '0;
-        in_sample_valid  <=  0;
-        out_sample_valid <=  0;
-    end else begin
-        // DATA_CLK_IN Sample rate clock divider
-        if (counter_in == SAMPLE_RATE_IN) begin
-            in_sample_valid <= 1'b1;
-            counter_in <= 0;
-        end else begin
-            in_sample_valid <= 1'b0;
-            counter_in <= counter_in + 1;
-        end
-        // DATA_CLK_OUT Sample rate clock divider
-        if (counter_out == SAMPLE_RATE_OUT) begin
-            out_sample_valid <= 1'b1;
-            counter_out <= 0;
-        end else begin
-            out_sample_valid <= 1'b0;
-            counter_out <= counter_out + 1;
-        end
-    end
-end
-
-// FIR pipeline registers and parameters
-localparam SUM_WIDTH = (2*IW) + $clog2(FILTER_LENGTH); // Prevent worst case overflow possibilities
-logic [IW-1:0] x[FILTER_LENGTH-1:0];                   // Array to store past input values
-logic [2*IW-1:0] mult_results[FILTER_LENGTH-1:0];      // Storing the multiplication results
-logic [SUM_WIDTH-1:0] sum_result, pipeline_sum;        // SUM_WIDTH-bit result after summing
-logic [IW-1:0] downsample_out;                         // Downsampled signals output, updates from sum_result at DATA_CLK_OUT rate
-
-// Systolic array logic
-logic [SUM_WIDTH-1:0] PE_out[FILTER_LENGTH];
-logic [SUM_WIDTH-1:0] PE_accum_in[FILTER_LENGTH], PE_accum_out[FILTER_LENGTH];
-
-// Instantiating PEs
-generate
-    for (int i = 0; i < FILTER_LENGTH; i++) begin: PE_INST
-        PE u_PE #(
-            .IW(IW),
-            .SUM_WIDTH(SUM_WIDTH)
-        )
-        (
-            .clk(clk),
-            .reset(reset),
-            .x(x[i]),
-            .coeff(coefficients[i]),
-            .accum_in(i == 0 ? 0 : PE_accum_out[i-1]),
-            .accum_out(PE_accum_out[i])
-        );
-    end
-endgenerate
-
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        data_out <= '0;
-        for (int i = FILTER_LENGTH-1; i >= 0; i = i-1)begin
+        counter <= 0;
+        sample_valid <= 0;
+        // Initialize sample storage registers to zero
+        for (int i = FILTER_LENGTH-1; i> 0; i = i-1)begin
             x[i] <= '0;
         end
+        // Initialize multiplication result registers to zero
+        for (int i = FILTER_LENGTH-1; i> 0; i = i-1)begin
+            mult_results[i] <= '0;
+        end
+        data_out <= 0;
     end else begin
-        // Input sampling
-        if (in_sample_valid) begin
-            // Shift old data values
-            x[0] <= data_in;
-            for(int i = 1; i < FILTER_LENGTH; i = i+1)
-                x[i] <= x[i-1];
+        // Counter Logic
+        if (counter == COUNTER_MAX) begin
+            sample_valid <= 1'b1;
+            counter <= 0;
+        end else begin
+            sample_valid <= 1'b0;
+            counter <= counter + 1;
         end
 
-        // Downsample to DATA_CLK_OUT sample rate
-        if (out_sample_valid) begin
-            data_out <= PE_accum_out[FILTER_LENGTH-1][SUM_WIDTH-1:SUM_WIDTH-IW] + (PE_accum_out[FILTER_LENGTH-1][SUM_WIDTH-IW-1] ? 1 : 0); // Right shift by IW-1 and round
+        if (sample_valid) begin
+            // Shift old data values
+            for(int i = FILTER_LENGTH-1; i > 0; i = i-1) begin
+                x[i] <= x[i-1];
+            end
+            x[0] <= data_in;
+
+            // Compute multiplication results
+            for(int i = 0; i < FILTER_LENGTH; i = i+1) begin
+                mult_results[i] <= x[i] * COEFFICIENTS[i];
+                // Sum the results
+                sum_result <= sum_result + mult_results[i];
+            end
+
+            // Downscale and round
+            data_out <= sum_result[31:16] + (sum_result[15] ? 1 : 0); // Right shift by 16 and round
         end
     end
 end
 
-endmodule
-
-
-// Processing Element (PE) for Systolic array
-module PE #(
-    parameter IW = 16,
-    parameter SUM_WIDTH = 32
-)
-(
-    input logic           clk,
-    input logic           reset,
-    input logic [IW-1:0]  x,
-    input logic [IW-1:0]  coeff,
-    input logic [SUM_WIDTH-1:0] accum_in,
-    output logic [SUM_WIDTH-1:0] accum_out
-);
-    logic [2*IW-1:0] mult_result;
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset)
-            accum_out <= 0;
-        else
-            accum_out <= accum_in + mult_result;
-    end
-    assign mult_result = x * coeff;
 endmodule
